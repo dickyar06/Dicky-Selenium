faf5cb7368bb7548389ffb9cf091ff89deaeb11e
dickyar06 <dickyabdulrachman6@gmail.com>
2024-05-24 19:16:53 +0700

    Register - UI Automation Test - Selenium Python

--- example.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-browser = webdriver.firefox()
-browser.get('https://demowebshop.tricentis.com/login')
-browser.find_element(by.NAME,'name')
---
+++ register.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Firefox()
+browser.get('https://demowebshop.tricentis.com/register')
+browser.find_element(By.CSS_SELECTOR,'input#gender-male').click()
+browser.find_element(By.NAME,'FirstName').send_keys('Dicky')
+browser.find_element(By.NAME,'LastName').send_keys('Rachman')
+browser.find_element(By.NAME,'Email').send_keys('dicky123@gmail.com')
+browser.find_element(By.NAME,'Password').send_keys('123456789')
+browser.find_element(By.NAME,'ConfirmPassword').send_keys('123456789')
+browser.find_element(By.NAME,'register-button').click()
+
+browser.get('https://demowebshop.tricentis.com/registerresult/1')
+browser.find_element(By.CLASS_NAME,'button').click()
