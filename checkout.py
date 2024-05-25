b5e2f52c781572b05cb2deb8d1a21cbf112132da
dickyar06 <dickyabdulrachman6@gmail.com>
2024-05-25 23:10:01 +0700

    update

--- checkout.py
+++ checkout.py
@@ -1,15 +1,22 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Firefox()
-browser.get('https://demowebshop.tricentis.com/login')
-browser.find_element(By.NAME,'Email').send_keys('dicky123@gmail.com')
-browser.find_element(By.NAME,'Password').send_keys('123456789')
-browser.find_element(By.CSS_SELECTOR,'.button-1.login-button').click()
+class demowebshopTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def test_login(self):
+        browser = self.browser
+        browser.get('https://demowebshop.tricentis.com/login')
+        browser.find_element(By.NAME,'Email').send_keys('dicky123@gmail.com')
+        browser.find_element(By.NAME,'Password').send_keys('123456789')
+        browser.find_element(By.CSS_SELECTOR,'.button-1.login-button').click()
+        browser.find_element(By.CSS_SELECTOR,'div:nth-of-type(3) > .product-item input[value="Add to cart"]').click()
+        browser.find_element(By.CSS_SELECTOR,'li#topcartlink  .cart-label').click()
+        browser.find_element(By.XPATH,'//input[@id="termsofservice"]').click()
+        browser.find_element(By.CSS_SELECTOR,'button#checkout').click('checkout')
 
-browser.find_element(By.CSS_SELECTOR,'div:nth-of-type(3) > .product-item input[value="Add to cart"]').click()
-browser.find_element(By.CSS_SELECTOR,'li#topcartlink  .cart-label').click()
-
-browser.find_element(By.NAME,'termsofservice').click()
-browser.find_element(By.NAME,'checkout').click()
+if __name__ == '__main__':
+    unittest.main()
