b8550bcfaad2c418af13e2d37dab084098bc36a4
dickyar06 <dickyabdulrachman6@gmail.com>
2024-05-26 14:23:50 +0700

    add billing address

--- checkout.py
+++ checkout.py
@@ -19,9 +19,17 @@ class demowebshopTest(unittest.TestCase):
         browser.find_element(By.CSS_SELECTOR,'li#topcartlink  .cart-label').click()
         
         browser.find_element(By.XPATH,'//input[@id="termsofservice"]').click()
-
         browser.find_element(By.XPATH,'//button[@id="checkout"]').click()
 
+        browser.find_element(By.NAME,'BillingNewAddress.Company').send_keys('warkop')
+        browser.find_element(By.NAME,'BillingNewAddress.City').send_keys('bandung')
+        browser.find_element(By.NAME,'BillingNewAddress.Address1').send_keys('bandung jawabarat')
+        browser.find_element(By.NAME,'BillingNewAddress.CountryId').send_keys('indonesia')
+        browser.find_element(By.NAME,'BillingNewAddress.ZipPostalCode').send_keys('40233')
+        browser.find_element(By.NAME,'BillingNewAddress.PhoneNumber').send_keys('081233444909')
+        
+        browser.find_element(By.XPATH,'//body/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/ol[1]/li[1]/div[2]/div[1]/input[1]').click()
+        
         webdriver.Chrome().close
 
 if __name__ == '__main__':
